e818a7f96b2ccfba265a1b0edffadbd54c628cf4
alecjacksn <alecajackson@gmail.com>
2017-10-27 16:37:10 -0600

    updated

--- javascriptYo.js
+++ javascriptYo.js
@@ -3,4 +3,7 @@ document.getElementById('worked').onclick = function(){
 }
 
 
-// web semantics:  the language of the web and the different ways it all communicates and works
+// web semantics:  adds meaning to language of the web.
+// best Practices: Sharing data,
+// the goal: to optomise the web to make it smarter. computers can help us get what we want. they will change from passivly helping us to activly helping us
+// purpose: it helps computers understand the meaning of a webpage rather than just putting text or syntax on a page
--- mastery-demo/src/router.js
+++ mastery-demo/src/router.js
@@ -5,12 +5,22 @@ import SetState from './components/SetState'
 import ReduxSection from './components/ReduxSection'
 import ServerPage from './components/ServerPage'
 
+const Test = ({ test }) => (
+    <div>
+        <h2>Home {test}</h2>
+    </div>
+)
+
 
 export default (
     <Switch>
-        <Route exact path ='/' component={HomePage}/>
-        <Route path ='/setstate' component={SetState}/>
-        <Route path ='/reduxsection' component={ReduxSection}/>
-        <Route path ='/serverpage' component={ServerPage}/>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/setstate' component={SetState} />
+        <Route path='/reduxsection' component={ReduxSection} />
+        <Route path='/serverpage' component={ServerPage} />
+        <Route path='/home' test={"Worksssss"}/>
+        <Route path='/there' render={(props) => {
+            return <div>You are here</div>
+        }} />
     </Switch>
 )
